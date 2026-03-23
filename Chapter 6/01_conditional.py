43e8861f02b8eb21d6d6c4ff36969d958159eac2
Prabhudayal <prabhudayaly005@gmail.com>
2026-03-23 23:44:30 +0530

    basic conditional statement

---
+++ Chapter 6/01_conditional.py
@@ -0,0 +1,11 @@
+a=int(input("Enetr your age:"))
+
+#if else statement
+if(a>=18):   #if condition is true.
+    print("You are the age of 18")
+    print("You are eligible to vote")
+
+else:        #otherwise
+    print("you are below the age of 18\nyou are not eligible to vote")
+
+print("end of the programme")
