9b2e01ce332646b9213e9bc3469b1fa1e0b706e1
Prabhudayal <prabhudayaly005@gmail.com>
2026-03-23 23:32:42 +0530

    focus on various function on sets datatype

---
+++ Chapter 5/04_set_methods.py
@@ -0,0 +1,12 @@
+sets={1,5,3,9,6,6,3,5,2,78,2,9,"prabhu"}
+
+sets.add(96) #this function add new data or element in set.
+print(sets)
+
+print(len(sets))
+
+sets.remove("prabhu") #it remocw the specifies element from set.
+print(sets)
+
+sets.clear() #it clear the set
+print(sets)
