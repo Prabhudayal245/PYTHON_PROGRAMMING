3128b8af14ad3af5549f42005f87f0e718a3c58b
Prabhudayal <prabhudayaly005@gmail.com>
2026-03-23 23:27:06 +0530

    creation of dictionary datatype

---
+++ Chapter 5/01_dictionary.py
@@ -0,0 +1,11 @@
+#Dictionary:It is a collection of keys,values pairs and it is mutable  
+dict={
+    "Name":"Prabhu",
+    "Marks":95,
+    "Roll_no":45,
+    "Branch":"COE",
+    "friends":["manish","saurabh","abhishek","arun"]
+    }
+
+print(dict,type(dict))
+print(dict["Marks"])  #dictionatry is already indexed.
