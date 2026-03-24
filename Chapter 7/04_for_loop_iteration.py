7fbc8653b45b4bb0a3cb6feabe0f8067ca7ba38f
Prabhudayal <prabhudayaly005@gmail.com>
2026-03-24 23:37:38 +0530

    learning iteration of for loop

---
+++ Chapter 7/04_for_loop_iteration.py
@@ -0,0 +1,19 @@
+L=["prabhu",39,93.50,True,None]
+print("content of list:")
+for i in L:
+    print(i)
+    
+print("\n")
+
+t=(896,"prabhu",True,29.39)
+print("content of tuple:")
+for i in t:
+    print(i)
+
+print("\n")
+
+s="prabhudayal"
+print("content of string:")
+for i in s:
+    print(i)
+
