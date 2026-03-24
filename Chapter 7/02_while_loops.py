9dfdd8c1f439f7d6ab12d672b0611ea5f8b2b5e5
Prabhudayal <prabhudayaly005@gmail.com>
2026-03-24 23:35:36 +0530

    understanding of while loops

---
+++ Chapter 7/02_while_loops.py
@@ -0,0 +1,19 @@
+# In while loops, the condition is checked first. If it evaluates to true, the body of the loop 
+# is executed otherwise not! 
+# If the loop is entered, the process of [condition check & execution] is continued until 
+# the condition becomes False. 
+
+i=1    #initialization of  i
+while(i<6):       #condition
+    print(i)    
+    i=i+1    #increment value of i by 1.
+    
+
+'''
+Output:
+1
+2
+3
+4
+5
+'''
