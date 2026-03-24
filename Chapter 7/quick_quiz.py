b9eb94ee5bbc26059e5ff8d13ea0a9c064ddfba6
Prabhudayal <prabhudayaly005@gmail.com>
2026-03-24 23:46:14 +0530

    quiz

---
+++ Chapter 7/quick_quiz.py
@@ -0,0 +1,15 @@
+#  Write a program to print 1 to 50 using a while loop.
+
+i=1  #initialization
+while(i<51):   #condition
+    print(i)
+    i=i+1      #increment the value of i by 1.
+
+L=["prabhu",39,93.50,True,None]
+print("conten of list is:")
+i=0
+while(i<len(L)):
+    print(L[i])
+    i=i+1
+
+print("end of programme")
