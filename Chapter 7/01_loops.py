b0f8f646b7a64cf3dd6196657d604337176b376c
Prabhudayal <prabhudayaly005@gmail.com>
2026-03-24 23:34:22 +0530

    understanding of loops

---
+++ Chapter 7/01_loops.py
@@ -0,0 +1,11 @@
+print("normal method")
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
+print("By using loop")
+
+# the same task can be done by using like this:
+for i in range(1,6):
+    print(i)
