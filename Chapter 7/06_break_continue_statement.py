48939a58f3cc34851e87324233198891e33bb29f
Prabhudayal <prabhudayaly005@gmail.com>
2026-03-24 23:45:47 +0530

    understanding of break and continue statement

---
+++ Chapter 7/06_break_continue_statement.py
@@ -0,0 +1,16 @@
+#Break statement:
+for i in range(100):
+    if(i==45):
+        break   #Exit the iteration right now!
+    print(i)
+
+#Continue statement:
+for  i in range(100):
+    if(i==45):
+        continue  #Skip the iteration
+    print(i)
+
+#Pass statement: it is a null statement in python
+for i in range(100):
+    pass #it instruct to do nothing.
+ 
