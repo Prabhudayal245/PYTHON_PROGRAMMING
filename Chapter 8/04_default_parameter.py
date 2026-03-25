df3b18e902897bfc96567f849facb3b9939275f6
Prabhudayal <prabhudayaly005@gmail.com>
2026-03-25 23:48:47 +0530

    add default paramenter

---
+++ Chapter 8/04_default_parameter.py
@@ -0,0 +1,6 @@
+def greet(name,ending="thank you"):
+    print(f"Good Day,{name}")
+    print(ending)
+
+greet("prabhu","thanks")
+greet("krish")   #if we cannot give paramenter value it takes by default the value in the function definition
