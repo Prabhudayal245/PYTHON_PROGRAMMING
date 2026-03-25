fa0fb36eb33818b8c981adab333e3da85e774884
Prabhudayal <prabhudayaly005@gmail.com>
2026-03-25 23:48:11 +0530

    foucs on return statement

---
+++ Chapter 8/03_function_return_value.py
@@ -0,0 +1,8 @@
+#function definition
+def greet(name,ending):
+    print("Good Day," + name)
+    print(ending)
+    return "ok,Good Bye!"
+
+a=greet("prabhu","thank you")   #return value "ok Good Bye!" can be stored into function call
+print(a)    #return value printed
