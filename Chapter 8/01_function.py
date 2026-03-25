5e0d4c3a426b65344e73c0f1f4129219d81976c9
Prabhudayal <prabhudayaly005@gmail.com>
2026-03-25 23:46:10 +0530

    define a funciton

---
+++ Chapter 8/01_function.py
@@ -0,0 +1,16 @@
+#Function:A function is a group of statement performing a  specific task.
+
+#the function can be called any number of times and anywhere in the programm.
+
+
+#Function definition
+def avg():
+    a=int(input("Enter the number:"))
+    b=int(input("Enter the number:"))
+    c=int(input("Enter the number:"))
+
+    average=(a+b+c)/3
+    print(average)
+
+avg()    #Function Call.
+
