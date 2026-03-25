959ad298ab274f405b45057d9bbcb753d32d8fdf
Prabhudayal <prabhudayaly005@gmail.com>
2026-03-25 23:46:55 +0530

    focus on arguments

---
+++ Chapter 8/02_function_with_argument.py
@@ -0,0 +1,8 @@
+def greet(name,ending):
+    print("Good Day!," + name)
+    print(ending)
+
+greet("prabhu","thanks")
+greet("rohan","thank you")
+greet("manav","thank you very much")
+greet("harsh","thanks")
