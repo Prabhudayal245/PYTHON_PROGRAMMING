c9ba017e874e39b448ac92747f89e1898a50ca6e
Prabhudayal <prabhudayaly005@gmail.com>
2026-03-25 23:50:02 +0530

    quiz

---
+++ Chapter 8/Quick_quiz.py
@@ -0,0 +1,10 @@
+#  Write a program to greet a user with “Good day” using functions.
+
+name=input("Enetr the name:")
+
+#Function definition
+def greet():
+    print(f"Good Day!,{name}")
+
+greet()   #Function Call
+
