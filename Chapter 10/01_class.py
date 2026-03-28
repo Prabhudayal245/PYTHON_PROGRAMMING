4864c55e4733331040d02e857277da4508db13da
Prabhudayal <prabhudayaly005@gmail.com>
2026-03-28 21:30:07 +0530

    adds class concepts of OOPs

---
+++ Chapter 10/01_class.py
@@ -0,0 +1,25 @@
+#class:class is an blueprint of object.
+
+#creating a class:
+class student:
+    #class attributes:
+    ClassTeacher="XYZ"       
+    Branch="COE"
+    subject=["maths","FDA","IKS","Physics","English"]
+    uniform="Pant Shirt"
+    bag="college bag"
+
+
+prabhu=student()  #object1
+prabhu.name="Prabhudayal"    #object/instance attributes 
+print(prabhu.name,prabhu.ClassTeacher,prabhu.Branch,prabhu.subject,prabhu.uniform)
+
+krish=student()   #object2
+krish.name="krish"   #object/insatance attributes
+print(krish.name,krish.ClassTeacher,krish.Branch,krish.subject,krish.uniform)
+
+
+
+
+    
+   
