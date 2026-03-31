828a6ad4e34414817e99f967d0d9691ef2c4b19d
Prabhudayal <prabhudayaly005@gmail.com>
2026-03-31 23:29:20 +0530

    add global and local variable examples in 10_global.py

---
+++ Chapter 12/10_global.py
@@ -0,0 +1,6 @@
+a=49   #global variable :are the variable which code can access this variable at anywhere in thee programm
+def num():
+    a=23   #local variable:are the variable which can use this variabe only inside this function.
+    print(a)
+num()
+print(a)
