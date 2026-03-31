334c905f99012f97cb38c1b159395bfcb5001c4b
Prabhudayal <prabhudayaly005@gmail.com>
2026-03-31 23:31:18 +0530

    add myfunc definition and main execution block in module.py

---
+++ Chapter 12/module.py
@@ -0,0 +1,10 @@
+def myfunc():
+    print("hello world")
+myfunc()
+
+
+if(__name__=="__main__"):
+    print("we are directly running on this code")
+    print(__name__)
+    
+#if(__name__=="__main__"): it means some parts of code which we do not want to run on the other file
